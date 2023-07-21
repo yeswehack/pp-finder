85fd95aba3465ccabe4122783a014f33d865f8db
SakiiR SakiiR <sakiir@protonmail.com>
2023-07-21 17:01:46 +0200

    removed non used options

--- src/cli.ts
+++ src/cli.ts
@@ -2,19 +2,11 @@
 import { run, subcommands } from "cmd-ts";
 
 import compile from "./cmds/compile";
-import hook from "./cmds/hook";
-import purge from "./cmds/purge";
-import rehook from "./cmds/rehook";
-import restore from "./cmds/restore";
 
 const app = subcommands({
     name: "PP Finder",
     description: "Find prototype pollution gadget in javascript code",
     cmds: {
-        hook,
-        rehook,
-        restore,
-        purge,
         compile
     },
 });
